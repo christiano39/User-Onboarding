f1b6fd0e9cbbe1630e09577c4ce87e4674c700dd
christiano39 <carneson39@gmail.com>
2020-06-17 13:20:24 -0600

    added id to users display

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 export default function User(props) {
-    const { name, email, password } = props.user
+    const { name, email, password, id } = props.user
 
     return (
         <div className='user'>
             <h3>{name}</h3>
+            <p>ID: {id}</p>
             <p>Email: {email}</p>
             <p>Password Length: {password.length} characters</p>
         </div>
